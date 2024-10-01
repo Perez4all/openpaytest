eca52027abfd36175ef10474089d7a72183deab6
Andres Perez <andres96666@gmail.com>
2024-09-30 21:19:40 -0500

    - Added authentication

---
+++ src/main/java/org/openpay/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package org.openpay.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.authorizeHttpRequests(req -> req.requestMatchers("/public/**","/auth/**").permitAll())
+                    .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                    .httpBasic(withDefaults());
+            return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        UserDetails admin = User.builder()
+                .username("andres")
+                .password(passwordEncoder().encode("openpay"))
+                .authorities("ROLE_ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
